49c169dc16668071d85a0fae23e64e9a85c4b29c
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-24 17:06:06 +0400

    perf: update JVM startup to use JAVA_OPTS, adjust memory settings and enable repository scanning

--- src/main/java/az/fitnest/catalog/CatalogServiceApplication.java
+++ src/main/java/az/fitnest/catalog/CatalogServiceApplication.java
@@ -13,6 +13,7 @@ package az.fitnest.catalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -20,6 +21,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @EnableJpaAuditing
 @EnableKafka
+@EnableJpaRepositories(basePackages = "az.fitnest.catalog.repository")
 public class CatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, (String[])args);
