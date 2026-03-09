37a0800415336c98dfe52d589eba43aa92787bbd
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-03-09 22:07:45 +0400

    fix: allow FitnestSecurityFilter to filter async dispatch for StreamingResponseBody

--- src/main/java/az/fitnest/catalog/security/FitnestSecurityFilter.java
+++ src/main/java/az/fitnest/catalog/security/FitnestSecurityFilter.java
@@ -22,6 +22,11 @@ import java.util.stream.Collectors;
 public class FitnestSecurityFilter extends OncePerRequestFilter {
 
     @Override
+    protected boolean shouldNotFilterAsyncDispatch() {
+        return false;
+    }
+
+    @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
