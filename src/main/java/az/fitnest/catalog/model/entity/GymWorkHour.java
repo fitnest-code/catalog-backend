723c9f178922412d4cecccadf7bdad3e9120a8b5
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-03-19 23:42:10 +0400

    refactor: replace List with Set for package options and update related logic

--- src/main/java/az/fitnest/catalog/model/entity/GymWorkHour.java
+++ src/main/java/az/fitnest/catalog/model/entity/GymWorkHour.java
@@ -15,7 +15,7 @@ import java.time.LocalTime;
 @Builder
 @Embeddable
 public class GymWorkHour {
-    private GymWorkHourPeriod period; // Only WEEKDAYS, SATURDAY, SUNDAY
+    private GymWorkHourPeriod period;
     private LocalTime fromTime;
     private LocalTime toTime;
 
