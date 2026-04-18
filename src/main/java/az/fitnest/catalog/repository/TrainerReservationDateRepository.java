8a73afc873b61d9a1e6213cddfdb5e23a22ac5d2
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-04-18 13:41:27 +0400

    chore: update JPQL query in TrainerReservationDateRepository for improved readability

--- src/main/java/az/fitnest/catalog/repository/TrainerReservationDateRepository.java
+++ src/main/java/az/fitnest/catalog/repository/TrainerReservationDateRepository.java
@@ -18,6 +18,6 @@ public interface TrainerReservationDateRepository extends JpaRepository<TrainerR
     
     List<TrainerReservationDate> findByTrainerIdAndDate(Long trainerId, LocalDate date);
     
-    @org.springframework.data.jpa.repository.Query("SELECT trd FROM TrainerReservationDate trd JOIN Trainer t ON trd.trainerId = t.id WHERE t.gymId = :gymId AND trd.date = :date")
+    @org.springframework.data.jpa.repository.Query("SELECT trd FROM TrainerReservationDate trd JOIN trd.trainer t WHERE t.gymId = :gymId AND trd.date = :date")
     List<TrainerReservationDate> findByGymIdAndDate(@org.springframework.data.repository.query.Param("gymId") Long gymId, @org.springframework.data.repository.query.Param("date") java.time.LocalDate date);
 }
