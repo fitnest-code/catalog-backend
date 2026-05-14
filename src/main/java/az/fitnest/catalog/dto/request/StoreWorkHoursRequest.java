40ab3ce7a5b18c5890c7928c9b46a2b6ec4974f7
nijataghayev <nicat.agayev.536@gmail.com>
2026-05-14 23:21:49 +0400

    create step 3 validasiya

--- src/main/java/az/fitnest/catalog/dto/request/StoreWorkHoursRequest.java
+++ src/main/java/az/fitnest/catalog/dto/request/StoreWorkHoursRequest.java
@@ -9,12 +9,10 @@ import lombok.Data;
 @Schema(description = "Mağazanın iş saatları")
 public class StoreWorkHoursRequest {
 
-    @NotBlank
     @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d$", message = "Format HH:mm olmalıdır")
     @Schema(description = "Açılış saatı", example = "09:00")
     private String from;
 
-    @NotBlank
     @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d$", message = "Format HH:mm olmalıdır")
     @Schema(description = "Bağlanış saatı", example = "22:00")
     private String to;
